668e62fb91a5c20c7ecefe6f0ec6c638b0374833
Aaron Weiss <aaronweiss74@gmail.com>
2013-01-09 19:57:38 -0500

    Cleaned up MotorComponent.

--- src/org/usfirst/frc1923/components/MotorComponent.java
+++ src/org/usfirst/frc1923/components/MotorComponent.java
@@ -1,64 +1,67 @@
-package org.usfirst.frc1923;
+package org.usfirst.frc1923.components;
 
+import org.usfirst.frc1923.Component;
 import edu.wpi.first.wpilibj.Victor;
 
 public class MotorComponent implements Component {
-
-	private int currentState = new Component.ComponentState().COMPONENT_OFF;
+	private int currentState = Component.ComponentState.COMPONENT_OFF;
 	private Victor victor;
-	
+
 	public MotorComponent(int channel) {
 		victor = new Victor(channel);
 	}
-	
-	public MotorComponent(int slot, int channel){
+
+	public MotorComponent(int slot, int channel) {
 		victor = new Victor(slot, channel);
 	}
-	
+
 	/**
 	 * @return the current state.
 	 */
-	
 	public int getState() {
 		return currentState;
 	}
-	
-	
+
 	/**
-	 * Sets the motor to forward with a specified speed. 
+	 * Sets the motor to forward with a specified speed.
+	 * 
 	 * @param speed
 	 */
-	
 	public void forward(int speed) {
 		victor.set(speed);
-		currentState = new Component.ComponentState().COMPONENT_FORWARD;
+		currentState = Component.ComponentState.COMPONENT_FORWARD;
 	}
-	
+
 	/**
 	 * Sets the motor to reverse with a specified speed.
+	 * 
 	 * @param speed
 	 */
-	
 	public void reverse(int speed) {
 		victor.set(speed);
-		currentState = new Component.ComponentState().COMPONENT_REVERSE;
+		currentState = Component.ComponentState.COMPONENT_REVERSE;
 	}
-	
+
 	/**
 	 * Get the recently set value of the PWM.
+	 * 
 	 * @return PWM value
 	 */
-	
 	public double get() {
 		return victor.get();
 	}
-	
+
 	/**
-	 *Set the PWM value.
-	 * @param speed between -1.0 and 1.0
+	 * Set the PWM value.
+	 * 
+	 * @param speed
+	 *            between -1.0 and 1.0
 	 */
-	
 	public void set(double speed) {
 		victor.set(speed);
+	}
+
+	public void destroy() {
+		victor.free();
 	}
 }
