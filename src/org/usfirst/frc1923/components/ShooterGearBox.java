adc32e7629d4219a6e76b373a82c4e43c8deff56
Pavan Hegde <pavan.hegde@rocketmail.com>
2013-01-21 19:22:57 -0500

    Re-wrote the ShooterGearbox class

--- src/org/usfirst/frc1923/components/ShooterGearBox.java
+++ src/org/usfirst/frc1923/components/ShooterGearBox.java
@@ -1,109 +1,152 @@
 package org.usfirst.frc1923.components;
 /**
- * @author Bhavish Yalamanchi
+ * A class for controlling the Shooter Gearbox.
+ * @author Pavan Hegde
  * @version 1.0
- * @since 1/19/13
+ * @since 1/21/13
  */
-public class ShooterGearBox {
+public class ShooterGearbox {
 	
-	private double[] leftGears;
-	private double[] rightGears;
-	private int leftGear = 0;
-	private int rightGear = 0;
-	private DriveComponent drive;
+	private double[] leftGearSet, rightGearSet;
+	private int leftGearNum, rightGearNum;
+	private MotorComponent motorComponent;
 	
-	public ShooterGearBox(double[] leftGears, double[] rightGears, DriveComponent drive) {
-		this.leftGears = leftGears;
-		this.rightGears = rightGears;
+	/**
+	 *  One of two constructors that set the above gear sets and motor component
+	 * @param leftGearNum The gear number for the left gear
+	 * @param rightGearNum The gear number for the right gear
+	 * @param leftGearSet the Left gear set
+	 * @param rightGearSet the right gear set
+	 * @param motorComponent the motor component
+	 */
+	public ShooterGearbox(double[] leftGearSet, double[] rightGearSet, MotorComponent motorComponent) {
+		this.rightGearSet = rightGearSet;
+		this.leftGearSet = leftGearSet;
+		this.motorComponent = motorComponent;
 	}
 	
-	public ShooterGearBox(int leftStart, int leftEnd, int increment, int rightStart, int rightEnd, DriveComponent drive) {
+	/**
+	 *  The second of two constructors that set the gear sets
+	 * @param leftStart The start of the range for the left
+	 * @param leftEnd The end of the range for the left
+	 * @param increment The increment used to set the difference.
+	 * @param rightStart The end of the range for the right
+	 * @param rightEnd The end of the range for the right
+	 * @param motorComponent the motor component
+	 */
+	public ShooterGearbox(int leftStart, int leftEnd, int increment, int rightStart, int rightEnd, MotorComponent motorComponent) {
 		int leftLength = ((leftEnd - leftStart) / increment) + 1;
-		this.leftGears = new double[leftLength];
+		this.leftGearSet = new double[leftLength];
 		for (int i = 0; i < leftLength; i++) {
-			leftGears[i] = (leftStart + (i * increment)) / 100;
+			leftGearSet[i] = (leftStart + (i * increment)) / 100;
 		}
 		int rightLength = ((rightEnd - rightStart) / increment) + 1;
-		this.rightGears = new double[rightLength];
+		this.rightGearSet = new double[rightLength];
 		for (int i = 0; i < rightLength; i++) {
-			rightGears[i] = (rightStart + (i * increment)) / 100;
+			rightGearSet[i] = (rightStart + (i * increment)) / 100;
 		}
-		this.drive = drive;
+		this.motorComponent = motorComponent;
 	}
-
+	/**
+	 *  Sets the left gear number
+	 * @param leftGearNum The gear number for the left
+	 */
+	public void setLeftGear(int leftGearNum) {
+		if (leftGearNum != this.leftGearNum && leftGearNum < leftGearSet.length && leftGearNum > 0) {
+			this.leftGearNum = leftGearNum;	
+			System.out.println("Shooter leftGear is changed to: " + this.leftGearNum);
+		}	
+	}
+	/**
+	 *  Sets the right gear number
+	 * @param rightGearNum The gear number for the right
+	 */
+	public void setRightGear(int rightGearNum) {
+		if (rightGearNum != this.rightGearNum && rightGearNum < rightGearSet.length && rightGearNum > 0) {
+			this.rightGearNum = rightGearNum;	
+			System.out.println("Shooter rightGear is changed to: " + this.rightGearNum);
+		}
+	}
+	/**
+	 * @return the left gear number
+	 */
 	public int getLeftGear() {
-		return leftGear;
+		return leftGearNum;
 	}
-
+	/**
+	 * @return the right gear number
+	 */
 	public int getRightGear() {
-		return rightGear;
+		return rightGearNum;
 	}
-	
-	public void setLeftGear(int leftGear) {
-		if (leftGear != this.leftGear && leftGear < leftGears.length && leftGear > 0) {
-			this.leftGear = leftGear;
-			System.out.println("Shooter Left Gear changed to: " + leftGear);
+	/**
+	 * Lowers right gear speed by one
+	 * @return the new gear number for the right
+	 */
+	public int rightGearDown () {
+		if (rightGearNum < (rightGearSet.length)) {
+			--rightGearNum;
+			this.motorComponent.set(rightGearSet[rightGearNum]);
 		}
-	}
-
-	public void setRightGear(int rightGear) {
-		if (rightGear != this.rightGear && rightGear < rightGears.length && rightGear > 0) {
-			this.rightGear = rightGear;
-			System.out.println("Shooter Right Gear changed to: " + rightGear);
+		else {
+			System.out.println("Right Shooter can't slow down any lower.");
 		}
+		return rightGearNum;
 	}
-	public int leftGearDown() {
-		if (leftGear < (leftGears.length - 1)) {
-			--leftGear;
-			drive.setMaxOutput(leftGears[leftGear]);
-			System.out.println("Shooter Left geared down to: " + leftGear);
+	/**
+	 * Lowers left gear speed by 1
+	 * @return the new left gear number.
+	 */
+	public int leftGearDown () {
+		if (leftGearNum < (leftGearSet.length)) {
+			--leftGearNum;
+			this.motorComponent.set(leftGearSet[leftGearNum]);
+			System.out.println("Left Shooter geared down to: " + leftGearNum);
 		} else {
-			System.out.println("Shooter Left cannot go any lower.");
+			System.out.println("Left Shooter can't slow down any lower.");
 		}
-		return leftGear;
+		return leftGearNum;
 	}
-	
-	public int rightGearDown() {
-		if (rightGear < (rightGears.length - 1)) {
-			--rightGear;
-			drive.setMaxOutput(rightGears[rightGear]);
-			System.out.println("Shooter Right geared down to: " + rightGear);
-		} else {
-			System.out.println("Shooter Right cannot go any lower");
-		}
-		return rightGear;
-	}
-	
-	public int leftGearUp() {
-		if (leftGear < (leftGears.length - 1)) {
-			++leftGear;
-			drive.setMaxOutput(leftGears[leftGear]);
-
-			System.out.println("Shooter Left geared up to: " + leftGear);
-		} else {
-			System.out.println("Shooter Left cannot go any higher.");
-		}
-		return leftGear;
-	}
-	
+	/**
+	 * Raises he right gear number by 1
+	 * @return the new right gear number
+	 */
 	public int rightGearUp() {
-		if (rightGear < (rightGears.length - 1)) {
-			++rightGear;
-			drive.setMaxOutput(rightGears[leftGear]);
-			System.out.println("Shooter Right geared up to: " + rightGear);
-		} else {
-			System.out.println("Shooter Right cannot go any higher.");
+		if (rightGearNum < (rightGearSet.length)) {
+			++rightGearNum;
+			this.motorComponent.set(rightGearSet[rightGearNum]);
+			System.out.println("Right Shooter geared up to: " + rightGearNum);
 		}
-		return rightGear;
+		else {
+			System.out.println("Right Shooter can't speed up any more.");
+		}
+		return rightGearNum;
 	}
-	
+	/**
+	 * Raises the left gear number by 1
+	 * @return the new left gear number
+	 */
+	public int leftGearUp() {
+		if (leftGearNum < (leftGearSet.length)) {
+			++leftGearNum;
+			this.motorComponent.set(leftGearSet[leftGearNum]);
+			System.out.println("Left Shooter geared up to: " + leftGearNum);
+		}
+		else {
+			System.out.println("Left Shooter can't speed up any more.");
+		}
+		return leftGearNum;
+	}
+	/**
+	 * @return Left gear speed
+	 */
 	public double getLeftSpeed() {
-		return leftGears[leftGear];
+		return leftGearSet[leftGearNum];
 	}
-	
+	/**
+	 * @return Right gear speed
+	 */
 	public double getRightSpeed() {
-		return rightGears[rightGear];
+		return rightGearSet[rightGearNum];
 	}
-	
-	
 }
