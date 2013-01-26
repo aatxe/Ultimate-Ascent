0c21242f1c2fcc1a879ec10336aefe98ef1b1502
Sid Senthilkumar <pikahero2@gmail.com>
2013-01-25 19:12:36 -0500

    Initialized and set boolean shooterRunning in ShooterComponenet

--- src/org/usfirst/frc1923/components/ShooterComponent.java
+++ src/org/usfirst/frc1923/components/ShooterComponent.java
@@ -5,7 +5,7 @@ import org.usfirst.frc1923.utils.JaguarGroup;
 public class ShooterComponent {
 	private JaguarGroup left;
 	private JaguarGroup right;
-	private boolean shooterRunning;
+	private boolean shooterRunning = false;
 	
 	public ShooterComponent(JaguarGroup left, JaguarGroup right) {
 		this.left = left;
@@ -14,15 +14,18 @@ public class ShooterComponent {
 	
 	public void runLeft(double leftSpeed) {
 		left.set(-Math.abs(leftSpeed));
+		shooterRunning = true;
 	}
 	
 	public void runRight(double rightSpeed) {
 		right.set(-Math.abs(rightSpeed));
+		shooterRunning = true;
 	}
 	
 	public void stop() {
 		left.disable();
 		right.disable();
+		shooterRunning = false;
 	}
 	
 	public boolean isShooterRunning() {
