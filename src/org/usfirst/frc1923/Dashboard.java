6bd73383f1a3edcca062f2fc4d4073659b962197
Aravind Koneru <aravind.b.koneru@gmail.com>
2013-02-03 14:22:11 -0500

    Completed the Smart Dashboard Class

--- src/org/usfirst/frc1923/Dashboard.java
+++ src/org/usfirst/frc1923/Dashboard.java
@@ -1,11 +1,9 @@
 package org.usfirst.frc1923;
 
-import org.usfirst.frc1923.events.DriveGearChangeEvent;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /**
  * Tells the user the values of the variables
- * @author Keshav Ramesh, Aravind Koneru, Bhavish, Viren Sawant
+ * @author Keshav Ramesh, Aravind Koneru, Bhavish, Viren Sawant, Pavan Hegde
  * @version 1.0
  * @since 2/3/13
  */
@@ -19,10 +17,14 @@ public class Dashboard {
 		SmartDashboard.putDouble("DriveTrainSpeed", 0);
 	}
 	
-	
-	
+	/**
+	 * Updates the Dashboard with the shooter speeds, drive speed, and tageting stuff.gg
+	 */
 	public void update(){
 		SmartDashboard.putDouble("DriveTrainSpeed",Components.driveGears.getSpeed());
+		SmartDashboard.putDouble("RightShooterSpeed", Components.shooterGears.getRightSpeed());
+		SmartDashboard.putDouble("LeftShooterSpeed", Components.shooterGears.getLeftSpeed());
+		//TODO Targeted and LightStatus
 		
 	}
 	
