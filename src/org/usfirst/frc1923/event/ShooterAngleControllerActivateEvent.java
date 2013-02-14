1e338f1f43b1803ef31c8a1266331765f9c6cb98
Aaron Weiss <aaronweiss74@gmail.com>
2013-02-14 14:31:09 -0500

    Added javadoc comments to ShooterAngleController events.

--- src/org/usfirst/frc1923/event/ShooterAngleControllerActivateEvent.java
+++ src/org/usfirst/frc1923/event/ShooterAngleControllerActivateEvent.java
@@ -2,6 +2,13 @@ package org.usfirst.frc1923.event;
 
 import org.usfirst.frc1923.Components;
 
+/**
+ * An event that activates the shooter angle controller.
+ * 
+ * @author Aaron Weiss
+ * @version 1.0
+ * @since 2/14/13
+ */
 public class ShooterAngleControllerActivateEvent extends Event {
 	/**
 	 * Creates the event.
--- src/org/usfirst/frc1923/event/ShooterAngleControllerDeactivateEvent.java
+++ src/org/usfirst/frc1923/event/ShooterAngleControllerDeactivateEvent.java
@@ -2,6 +2,13 @@ package org.usfirst.frc1923.event;
 
 import org.usfirst.frc1923.Components;
 
+/**
+ * An event that deactivates the shooter angle controller.
+ * 
+ * @author Aaron Weiss
+ * @version 1.0
+ * @since 2/14/13
+ */
 public class ShooterAngleControllerDeactivateEvent extends Event {
 	/**
 	 * Creates the event.
